a57ecad59b772d19eefbba1063f20deeeb7d5d5c
hossain1527 <hossain.sci2015@gmail.com>
2020-10-08 01:48:30 +0100

    Modified with some stuff

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let btn = document.getElementById('btn');
 let result = document.getElementById('output');
 let inputF = document.getElementById('inputF');
@@ -21,4 +20,58 @@ btn.addEventListener('click', () =>{
   
  
 });
+
+const allTabs = document.querySelectorAll('[data-tab-target]');
+const contents = document.querySelectorAll('[data-tab-content]');
+
+allTabs.forEach(singleTab => {
+  singleTab.addEventListener('click', () => {
+    const target = document.querySelector(singleTab.dataset.tabTarget);
+    contents.forEach(content => {
+      content.classList.remove('active');
+      
+    });
+    target.classList.add('active');
+    
+  });
+});
+
+function tick() {
+  var chbx = document.getElementsByClassName('chbx');
+
+   if(chbx[2].checked) {
+    document.getElementById('crnt').style.textDecoration = 'line-through';
+  
+  } else{
+     document.getElementById('crnt').style.textDecoration = 'none';
+  }
+}
+
+function tick1() {
+  
+  var chbx1 = document.getElementsByClassName('chbx1');
+
+  if(chbx1[0].checked) {
+    document.getElementById('newSs').style.textDecoration = 'line-through';
+    
+  }else {
+    document.getElementById('newSs').style.textDecoration = 'none';
+  }
+
+  if(chbx1[1].checked) {
+    document.getElementById('newComp').style.textDecoration = 'line-through';
+    
+  }else {
+    document.getElementById('newComp').style.textDecoration = 'none';
+  }
+
+  if(chbx1[2].checked) {
+    document.getElementById('newCrnt').style.textDecoration = 'line-through';
+    
+  }else {
+    document.getElementById('newCrnt').style.textDecoration = 'none';
+  }
+}
+
+
 
